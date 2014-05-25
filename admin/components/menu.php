0c1194d2be77c30e6f57370a4da69bb7df6ad330
xumeng <xumeng0321@gmail.com>
2014-05-25 11:58:19 +0800

    后台商品管理页面商品展示

--- admin/components/menu.php
+++ admin/components/menu.php
@@ -1,7 +1,7 @@
 
 	<ul>
 		<li><a href="typeController.php">商品分类</a></li>
-		<li>商品管理</li>
+		<li><a href="goodsController.php">商品管理</a></li>
 		<li><a href="userController.php">用户管理</a></li>
 		<li>公告管理</li>
 	</ul>
